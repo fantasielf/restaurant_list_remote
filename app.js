8d8cd31ffc1238230558a0315d5d103964b767eb
fantasielf <ben1976@livemail.tw>
2020-09-30 12:30:31 +0800

    project in

---
+++ app.js
@@ -0,0 +1,36 @@
+// app.js
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find((restaurant) => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.result.filter((restaurant) => {
+    return restaurant.name, toLowCase().includes(keyword.toLowCase())
+  })
+  res.render('index', { restaurant: restaurants, keyword: keyword })
+})
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+
+// setting static files
+app.use(express.static('public'))
